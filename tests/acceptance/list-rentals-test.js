71af13a59c5cba19d2f92a04433591d2283adff6
Hoang Chau Khanh <Khanh.HoangChau@vn.bosch.com>
2016-07-27 16:18:09 +0700

    Add index, contact and about routes

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,39 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /list-rentals', function(assert) {
-  visit('/list-rentals');
+test('should list available rentals', (assert) => {
+  visit('/');
 
-  andThen(function() {
-    assert.equal(currentURL(), '/list-rentals');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see 3 listing tags');
+  })
+});
+
+test('should link to information about company', (assert) => {
+  visit('/');
+  click('a:contains("About")');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+
+test('should link to contact information', (assert) => {
+  visit('/');
+  click('a:contains("Contact")');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+
+test('should filter the list of rentals by city', function (assert) {
+  visit('/');
+  fillIn('.list-filter input', 'seattle');
+  keyEvent('.list-filter input', 'keyup', 69);
+
+  andThen(() => {
+    assert.equal(find('.listing').length, 1, 'should show 1 rentals in list');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
   });
 });
