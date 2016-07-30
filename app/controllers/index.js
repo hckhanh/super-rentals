a2c69b3d1d3daeb16f250f11c80b8c415f82f87e
Travis <travis@travis-ci.org>
2016-07-30 13:46:44 +0700

    Update rentals data

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
+import ENV from 'super-rentals/config/environment';
 
 export default Ember.Controller.extend({
   actions: {
     filterByCity(city) {
+      if (ENV.APP.RENTALS_DATA) {
+        return Ember.RSVP.resolve(city !== '' ?
+          ENV.APP.RENTALS_DATA.filter(rental =>
+            rental.city.toLowerCase().indexOf(city.toLowerCase()) !== -1)
+          : ENV.APP.RENTALS_DATA);
+      }
+
       return city !== '' ?
         this.get('store').query('rental', { city: city })
         : this.get('store').findAll('rental');
--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 
-module.exports = function(environment) {
+const rentals = [{
+  id: 1,
+  title: 'Grand Old Mansion',
+  owner: 'Veruca Salt',
+  city: 'San Francisco',
+  type: 'Estate',
+  bedrooms: 15,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+}, {
+    id: 2,
+    title: 'Urban Living',
+    owner: 'Mike TV',
+    city: 'Seattle',
+    type: 'Condo',
+    bedrooms: 1,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+  }, {
+    id: 3,
+    title: 'Downtown Charm',
+    owner: 'Violet Beauregarde',
+    city: 'Portland',
+    type: 'Apartment',
+    bedrooms: 3,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+  }];
+
+module.exports = function (environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
@@ -39,7 +65,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.APP.RENTALS_DATA = rentals;
   }
 
   return ENV;
